5f906cd20aaf9e6c8ef86ee70e09bb1db1cd9516
FranGarciaga <frangg_10@hotmail.com>
2024-11-26 15:14:51 +0100

    feat: fixing relations between class

--- src/main/java/org/example/logic/Mentor.java
+++ src/main/java/org/example/logic/Mentor.java
@@ -2,6 +2,8 @@ package org.example.logic;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name="Mentor")
 public class Mentor {
@@ -11,15 +13,28 @@ public class Mentor {
     private int id;
     @Column(name = "name")
     private String name;
+    @Column(name = "surname")
+    private String surname;
     @Column(name="phone")
     private int phone;
 
-    public Mentor(int id, String name, int phone) {
+    /*@OneToMany(mappedBy = "mentor", cascade = CascadeType.ALL)
+    private List<Patient> patients;*/
+
+    public Mentor(int id, String name, String surname, int phone) {
         this.id = id;
         this.name = name;
+        this.surname = surname;
         this.phone = phone;
     }
 
+    public String getSurname() {
+        return surname;
+    }
+
+    /*public List<Patient> getPatients() {
+        return patients;
+    }*/
 
     public int getId() {
         return id;
--- src/main/java/org/example/logic/Patient.java
+++ src/main/java/org/example/logic/Patient.java
@@ -18,13 +18,21 @@ public class Patient {
     @Column(name="age")
     private int age;
 
+    @ManyToOne
+    @JoinColumn(name = "mentor_id")
+    private Mentor mentor;
+
     public Patient(String name, String species, String race, int age) {
         this.name = name;
         this.species = species;
         this.race = race;
         this.age = age;
+
     }
 
+    public Mentor getMentor() {
+        return mentor;
+    }
 
     public int getId() {
         return id;
