c72acc4a18457243f742380b3ece5515addaf2e9
FranGarciaga <frangg_10@hotmail.com>
2024-11-27 15:03:40 +0100

    feat: Test givenMentorById return success

--- src/test/java/org/example/controllers/MentorControllerTest.java
+++ src/test/java/org/example/controllers/MentorControllerTest.java
@@ -17,34 +17,61 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 class MentorControllerTest {
-    
+
     @Autowired
     private MentorRepository mentorRepository;
-    
+
     @Autowired
     MockMvc mockMvc;
-    
+
     @Test
     void givenAValidMentor_whenSaving_thenReturnSuccess() throws Exception {
         Mentor mentor1 = new Mentor("Fran", "Perez", "123456789");
         mentorRepository.save(mentor1);
-        
+
         String jsonreponse =
-              """
-                          [
-                              {
-                                  "id": 1,
-                                  "name": "Fran",
-                                  "surname": "Perez",
-                                  "phone": 123456789
-                              }
-                          ]
-              """;
-        
+                """
+                                    [
+                                        {
+                                            "id": 1,
+                                            "name": "Fran",
+                                            "surname": "Perez",
+                                            "phone": "123456789"
+                                        }
+                                    ]
+                        """;
+
         mockMvc.perform(get("/mentors").contentType(MediaType.APPLICATION_JSON))
-              .andExpect(status().isOk())
-              .andExpect(content().json(jsonreponse));
-        
+                .andExpect(status().isOk())
+                .andExpect(content().json(jsonreponse));
+
     }
+
+    @Test
+    void givenMentorById_whenSearch_thenReturnSuccess() throws Exception {
+        Mentor mentor = new Mentor("Paloma", "Perez", "987654321");
+        Mentor mentor1 = new Mentor("Palom", "Perez", "987654321");
+        Mentor mentor2 = new Mentor("Palo", "Perez", "987654321");
+        mentorRepository.save(mentor);
+        mentorRepository.save(mentor1);
+        mentorRepository.save(mentor2);
+
+        String jsonreponse =
+                """
+                        
+                                        {
+                                            "id": 1,
+                                            "name": "Paloma",
+                                            "surname": "Perez",
+                                            "phone": "987654321"
+                                        }
+                        
+                        """;
+        mockMvc.perform(get("/mentors/1", mentor1.getId()).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(jsonreponse));
+
+    }
+
 
 }
